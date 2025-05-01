f5425e25a64073a2add20eaa4973c8b64c267631
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-05-01 16:24:50 +0200

    JSFix Done

---
+++ js/main.mjs
@@ -0,0 +1,37 @@
+import {FileInserter} from "/js/FileInserter.mjs";
+
+/*function addTopBar() {
+    var inserter = new JSONInserter();
+    inserter.set("topBar", "/assets/JSON/topBar.json");
+    inserter.insert();
+}*/
+
+async function addTopBar() {
+  var inserter = new FileInserter();
+  await inserter.set("top-bar", "/assets/content/topBar.html");
+  inserter.insert();
+}
+async function addContents() {
+  var inserter = new FileInserter();
+  await inserter.set("pictures", "/assets/content/pictures.html");
+  await inserter.set("menu", "/assets/content/menu.html");
+  await inserter.set("location", "/assets/content/location.html");
+  await inserter.set("data", "assets/content/data.html");
+  inserter.insert();
+}
+
+/*function fillInData() {
+    $.ajax({
+        async: false,
+        url: "/assets/JSON/data.json",
+        success: function(jd) {
+            var inserter = new Inserter();
+            inserter.ignoreNotPresent = true;
+            inserter.set("businessName", jd.data.name);
+            inserter.insert();
+        }
+    });
+}*/
+
+addTopBar();
+addContents();
