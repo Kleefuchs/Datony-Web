e3c2f630e01eef2366024d0f8e7cbcd68f50307c
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-05-02 08:38:56 +0200

    Silly Pictures

--- assets/content/js/pictures.mjs
+++ assets/content/js/pictures.mjs
@@ -1,1 +1,9 @@
-import { imageviewer } from "/assets/content/js/imageviewer.mjs";
+import { ImageViewer } from "/assets/content/js/imageviewer.mjs";
+
+async function initImageViewer() {
+  var imageViewer = new ImageViewer();
+  console.info("Test");
+  await imageViewer.loadImages("/assets/content/json/selected-for-view.json");
+}
+
+initImageViewer();
